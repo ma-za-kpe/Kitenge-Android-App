13b48a42895aa883a77bc22dc9357a4b3416e9f4
MAZAKPE <makpalyy@gmail.com>
2020-02-17 08:15:14 +0200

    added in app-update to the app

--- app/src/main/java/com/maku/kitenge/ui/MainActivity.kt
+++ app/src/main/java/com/maku/kitenge/ui/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.maku.kitenge.ui
 
 import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import android.content.IntentSender.SendIntentException
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -47,9 +50,28 @@ class MainActivity : AppCompatActivity() {
         )
 
         checkUpdate()
-
+//        createNotificationChannel()
     }
 
+    /*create notification*/
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val name = getString(R.string.channel_name)
+//            val descriptionText = getString(R.string.channel_description)
+//            val importance = NotificationManager.IMPORTANCE_DEFAULT
+//            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+//                description = descriptionText
+//            }
+//            // Register the channel with the system
+//            val notificationManager: NotificationManager =
+//                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//        }
+    }
+
+    /* check updates */
     private fun checkUpdate() {
 
         // Before starting an update, register a listener for updates.
@@ -131,7 +153,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun popupSnackbarForCompleteUpdate() {
         val snackbar = Snackbar.make(
-            findViewById(R.id.snack),
+            findViewById(android.R.id.content),
             "An update has just been downloaded.",
             Snackbar.LENGTH_INDEFINITE
         )
